35947af4bdb7b31da337d71b327991f529067986
Kirill Ryabikin <98740566+Kilvii@users.noreply.github.com>
2024-04-17 23:36:58 +0300

    Edit Card Component

--- TodoList/src/App.vue
+++ TodoList/src/App.vue
@@ -12,6 +12,7 @@ export default {
   },
   setup() {
     const header = 'Список дел';
+    const strEmptyList = 'Список пуст';
     const newItem = ref('');
     const items = ref([]);
 
@@ -24,6 +25,7 @@ export default {
 
     return {
       header,
+      strEmptyList,
       newItem,
       items,
       addItem,
@@ -37,34 +39,50 @@ export default {
     <div class="header">
       <h1>{{ header }}</h1>
     </div>
-    <InputComponent 
-    v-model.trim="newItem" 
-    @keyup.enter="addItem" 
-    type="text"
-    height="20px"
-    width="200px"
-    placeholder="Поиск"
-     />
-    <ObjectCardComponent 
-    :items="items"
-    height="80px"
-    width="260px"
-    color="black"
-    backgroundColor="white"
-    />
+    <InputComponent v-model.trim="newItem" @keyup.enter="addItem" type="text" height="20px" width="200px"
+      placeholder="Поиск" />
+    <div class="cards">
+      <ul class="cards-list">
+        <li v-for="(item, index) in items" :key="index">
+          <ObjectCardComponent :item="item" height="80px" width="260px" color="black" backgroundColor="white" />
+        </li>
+      </ul>
+      <p v-if="!items.length" class="empty-list">
+        {{ strEmptyList }}
+      </p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center; 
-    justify-content: center; 
-    height: 100vh; 
-  }
-  
-  .header {
-    margin-bottom: 20px;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.header {
+  margin-bottom: 20px;
+}
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  align-items: center;
+  justify-content: center;
+}
+
+.cards-list {
+  padding-left: 0;
+  margin-bottom: 20px;
+  list-style-type: none;
+}
+
+.empty-list {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
 </style>
--- TodoList/src/components/ObjectCardComponent.vue
+++ TodoList/src/components/ObjectCardComponent.vue
@@ -1,14 +1,13 @@
 <script setup>
 import ButtonComponent from './ButtonComponent.vue';
-import InputComponent from './InputComponent.vue';
 
 const props = defineProps({
   label: {
     type: String,
     default: "",
   },
-  items: {
-    type: Array,
+  item: {
+    type: String,
     requaired: true,
   },
   color: {
@@ -45,46 +44,18 @@ const deleteItem = (index) => {
 </script>
 
 <template>
-  <div class="cards">
-    <ul class="card-list">
-      <li v-for="(item, index) in items" :key="index">
-        <div class="item" :style="{ color: color, backgroundColor: backgroundColor, height: height, width: width }">
-          <span>{{ item }}</span>
-          <ButtonComponent background-color="red" height="40px" width="46px" @action="editItem(index)"
-            icon="./icons/Add.svg">
-          </ButtonComponent>
-          <ButtonComponent background-color="green" height="40px" width="46px" @action="deleteItem(index)"
-            icon="./icons/Delete.svg">
-          </ButtonComponent>
-        </div>
-      </li>
-    </ul>
-    <p v-if="!items.length" class="empty-list">
-      Список пуст
-    </p>
+  <div class="card" :style="{ color: color, backgroundColor: backgroundColor, height: height, width: width }">
+    <span>{{ item }}</span>
+    <ButtonComponent background-color="red" height="40px" width="46px" @action="editItem(index)" icon="./icons/Add.svg">
+    </ButtonComponent>
+    <ButtonComponent background-color="green" height="40px" width="46px" @action="deleteItem(index)"
+      icon="./icons/Delete.svg">
+    </ButtonComponent>
   </div>
 </template>
 
 <style scoped>
-/* ::v-deep .btn {
-  margin-right: 0px;
-} */
-
-.cards {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-  align-items: center;
-  justify-content: center;
-}
-
-.card-list {
-  padding-left: 0;
-  margin-bottom: 20px;
-  list-style-type: none;
-}
-
-.item {
+.card {
   font-size: 16px;
   display: flex;
   align-items: center;
@@ -95,12 +66,7 @@ const deleteItem = (index) => {
   justify-content: center;
 }
 
-.item span {
+.card span {
   padding-right: 10px;
-}
-
-.empty-list {
-  font-size: 16px;
-  margin-bottom: 10px;
 }
 </style>
